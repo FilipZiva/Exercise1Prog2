40728b6d3cf04e48805b1a53e2e9ca0a2afb9d20
Camono <ivan.ban@stud.fh-campuswien.ac.at>
2024-06-04 16:49:30 +0200

    added observable classes

---
+++ src/main/java/at/ac/fhcampuswien/fhmdb/dao/Observable.java
@@ -0,0 +1,10 @@
+package at.ac.fhcampuswien.fhmdb.dao;
+
+public interface Observable<T> {
+
+    void registerObserver(Observer<T> observer);
+
+    void unregisterObserver(Observer<T> observer);
+
+    void notifyObservers(T data);
+}
---
+++ src/main/java/at/ac/fhcampuswien/fhmdb/dao/Observer.java
@@ -0,0 +1,6 @@
+package at.ac.fhcampuswien.fhmdb.dao;
+
+public interface Observer<T> {
+
+    void update(T data);
+}
